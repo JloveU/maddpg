1893eb0073e55820ecdd0b6b478d1b18e61efbe6
yuqiang <932967248@qq.com>
2019-01-14 15:43:20 +0800

    Add support for tensorflow older than 1.5

--- maddpg/common/tf_util.py
+++ maddpg/common/tf_util.py
@@ -3,25 +3,50 @@ import numpy as np
 import os
 import tensorflow as tf
 
-def sum(x, axis=None, keepdims=False):
-    return tf.reduce_sum(x, axis=None if axis is None else [axis], keep_dims = keepdims)
-def mean(x, axis=None, keepdims=False):
-    return tf.reduce_mean(x, axis=None if axis is None else [axis], keep_dims = keepdims)
-def var(x, axis=None, keepdims=False):
-    meanx = mean(x, axis=axis, keepdims=keepdims)
-    return mean(tf.square(x - meanx), axis=axis, keepdims=keepdims)
-def std(x, axis=None, keepdims=False):
-    return tf.sqrt(var(x, axis=axis, keepdims=keepdims))
-def max(x, axis=None, keepdims=False):
-    return tf.reduce_max(x, axis=None if axis is None else [axis], keep_dims = keepdims)
-def min(x, axis=None, keepdims=False):
-    return tf.reduce_min(x, axis=None if axis is None else [axis], keep_dims = keepdims)
-def concatenate(arrs, axis=0):
-    return tf.concat(axis=axis, values=arrs)
-def argmax(x, axis=None):
-    return tf.argmax(x, axis=axis)
-def softmax(x, axis=None):
-    return tf.nn.softmax(x, axis=axis)
+tf_major_version, tf_minor_version = int(tf.__version__.split(".")[0]), int(tf.__version__.split(".")[1])
+
+
+if (tf_major_version >= 1) and (tf_minor_version >= 5):
+    def sum(x, axis=None, keepdims=False):
+        return tf.reduce_sum(x, axis=None if axis is None else [axis], keep_dims=keepdims)
+    def mean(x, axis=None, keepdims=False):
+        return tf.reduce_mean(x, axis=None if axis is None else [axis], keep_dims=keepdims)
+    def var(x, axis=None, keepdims=False):
+        meanx = mean(x, axis=axis, keepdims=keepdims)
+        return mean(tf.square(x - meanx), axis=axis, keepdims=keepdims)
+    def std(x, axis=None, keepdims=False):
+        return tf.sqrt(var(x, axis=axis, keepdims=keepdims))
+    def max(x, axis=None, keepdims=False):
+        return tf.reduce_max(x, axis=None if axis is None else [axis], keep_dims=keepdims)
+    def min(x, axis=None, keepdims=False):
+        return tf.reduce_min(x, axis=None if axis is None else [axis], keep_dims=keepdims)
+    def concatenate(arrs, axis=0):
+        return tf.concat(axis=axis, values=arrs)
+    def argmax(x, axis=None):
+        return tf.argmax(x, axis=axis)
+    def softmax(x, axis=None):
+        return tf.nn.softmax(x, axis=axis)
+else:
+    def sum(x, axis=None, keepdims=False):
+        return tf.reduce_sum(x, dim=None if axis is None else [axis], keep_dims=keepdims)
+    def mean(x, axis=None, keepdims=False):
+        return tf.reduce_mean(x, dim=None if axis is None else [axis], keep_dims=keepdims)
+    def var(x, axis=None, keepdims=False):
+        meanx = mean(x, axis=axis, keepdims=keepdims)
+        return mean(tf.square(x - meanx), axis=axis, keepdims=keepdims)
+    def std(x, axis=None, keepdims=False):
+        return tf.sqrt(var(x, axis=axis, keepdims=keepdims))
+    def max(x, axis=None, keepdims=False):
+        return tf.reduce_max(x, dim=None if axis is None else [axis], keep_dims=keepdims)
+    def min(x, axis=None, keepdims=False):
+        return tf.reduce_min(x, dim=None if axis is None else [axis], keep_dims=keepdims)
+    def concatenate(arrs, axis=0):
+        return tf.concat(dim=axis, values=arrs)
+    def argmax(x, axis=None):
+        return tf.argmax(x, dim=axis)
+    def softmax(x, axis=None):
+        return tf.nn.softmax(x, dim=axis)
+
 
 # ================================================================
 # Misc
